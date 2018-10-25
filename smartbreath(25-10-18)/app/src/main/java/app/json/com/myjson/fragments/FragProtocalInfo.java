705feedbccb768c45c2b47328d750724ce2a730b
Nandinisiddhi <nandini.k@siddhiworld.com>
2018-10-25 18:25:24 +0530

    25-10-2018 SmartBreath Apllication

---
+++ smartbreath(25-10-18)/app/src/main/java/app/json/com/myjson/fragments/FragProtocalInfo.java
@@ -0,0 +1,24 @@
+package app.json.com.myjson.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import app.json.com.myjson.R;
+
+
+public class FragProtocalInfo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.protocalinfofrag, container, false);
+
+        return view;
+    }
+}
---
+++ smartbreath(25-10-18)/app/src/main/java/app/json/com/myjson/fragments/ProtocolPage2.java
@@ -0,0 +1,22 @@
+package app.json.com.myjson.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.json.com.myjson.R;
+
+public class ProtocolPage2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.protocolpage2frag, container, false);
+
+        return view;
+    }
+}
