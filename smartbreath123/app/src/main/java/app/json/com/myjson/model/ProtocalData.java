ed5abce81e3911fe51545dff8c974675c769dde4
bhuvaneswarisiddhi <bhuvaneswari.p@siddhiworld.com>
2018-11-14 19:33:48 +0530

    smartbreath working on code

---
+++ smartbreath123/app/src/main/java/app/json/com/myjson/model/ProtocalData.java
@@ -0,0 +1,98 @@
+package app.json.com.myjson.model;
+
+
+
+public class ProtocalData {
+
+    String pName;
+    String pDescription;
+    String pNoOfStages;
+    String pSuitablePersonality;
+
+    String sName;
+    String sPositive;
+    String sNegative;
+    String saltitude;
+    String sMethod;
+    String sDescription;
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpDescription() {
+        return pDescription;
+    }
+
+    public void setpDescription(String pDescription) {
+        this.pDescription = pDescription;
+    }
+
+    public String getpNoOfStages() {
+        return pNoOfStages;
+    }
+
+    public void setpNoOfStages(String pNoOfStages) {
+        this.pNoOfStages = pNoOfStages;
+    }
+
+    public String getpSuitablePersonality() {
+        return pSuitablePersonality;
+    }
+
+    public void setpSuitablePersonality(String pSuitablePersonality) {
+        this.pSuitablePersonality = pSuitablePersonality;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getsPositive() {
+        return sPositive;
+    }
+
+    public void setsPositive(String sPositive) {
+        this.sPositive = sPositive;
+    }
+
+    public String getsNegative() {
+        return sNegative;
+    }
+
+    public void setsNegative(String sNegative) {
+        this.sNegative = sNegative;
+    }
+
+    public String getSaltitude() {
+        return saltitude;
+    }
+
+    public void setSaltitude(String saltitude) {
+        this.saltitude = saltitude;
+    }
+
+    public String getsMethod() {
+        return sMethod;
+    }
+
+    public void setsMethod(String sMethod) {
+        this.sMethod = sMethod;
+    }
+
+    public String getsDescription() {
+        return sDescription;
+    }
+
+    public void setsDescription(String sDescription) {
+        this.sDescription = sDescription;
+    }
+}
